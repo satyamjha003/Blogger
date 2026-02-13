04a5efcd932f77f982050bf38e16fb4ef5e69e72
satyamjha003 <satyamkumarspj2006@gmail.com>
2026-02-13 16:16:54 +0530

    added getSinglePost function in postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -72,3 +72,21 @@ export const deletePost = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Get single post
+export const getSinglePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate(
+      "author",
+      "username email"
+    );
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, getAllPosts, updatePost, deletePost } from "../controllers/postController.js";
+import { createPost, getAllPosts, updatePost, deletePost, getSinglePost } from "../controllers/postController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/", protect, createPost); // protected
 router.get("/", getAllPosts);           // public
 router.put("/:id", protect, updatePost); // protected
 router.delete("/:id", protect, deletePost); // protected
+router.get("/:id", getSinglePost);
 
 export default router;
