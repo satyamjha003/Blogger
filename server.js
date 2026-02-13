e662384ce3f10e1dee13049299d81583bb5e4d77
satyamjha003 <satyamkumarspj2006@gmail.com>
2026-02-13 16:16:09 +0530

    Added show post route and page

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
